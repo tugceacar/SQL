1b888829469950ecc5a745dab7b891d6681a73c5
tugceacar <tugceacar.ozkan@gmail.com>
2021-08-18 15:38:19 -0400

    updated..

---
+++ src/TugceA.java
@@ -0,0 +1,10 @@
+public class TugceA {
+
+    public static void main(String[] args) {
+        System.out.println("Sql class..");
+        System.out.println("Sql class..");
+        System.out.println("Sql class..");
+        System.out.println("Sql class..");
+        System.out.println("Sql class..");
+    }
+}
